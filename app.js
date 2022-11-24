4231f7a6b5efeea16c2b0b6641226d4fff9b68c6
linvaldo_rodrigues <linvaldo.rodrigues10@gmail.com>
2022-11-24 08:39:53 -0300

    botão_remover_despesa

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ class BancoDeDados {
       if (despesa === null) {
         continue;
       }
+      despesa.id = i
       despesas.push(despesa);
     }
 
@@ -138,6 +139,10 @@ class BancoDeDados {
     console.log(despesasFiltradas);
     return despesasFiltradas;
     
+  }
+
+  remover(id){
+    localStorage.removeItem(id)
   }
 }
 
@@ -237,6 +242,22 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
     linha.insertCell(1).innerHTML = d.tipo;
     linha.insertCell(2).innerHTML = d.descricao;
     linha.insertCell(3).innerHTML = d.valor;
+
+    //botão de exclusão
+    let btn = document.createElement('button')
+    btn.className = 'btn btn-danger'
+    btn.innerHTML = '<i class = "fas fa-times"></i>'
+    btn.id = `id_despesa_${d.id}`
+
+    btn.onclick = function(){
+      let id = this.id.replace('id_despesa_', '')
+      //alert(id)
+      Bd.remover(id)
+      window.location.reload()
+    }
+
+    linha.insertCell(4).append(btn)
+    console.log(d)
   });
 }
 
